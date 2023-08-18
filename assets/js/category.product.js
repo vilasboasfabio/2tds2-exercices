3a7890940b03d2835ba5425e5c4dd42310c06969
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-08-18 15:41:21 -0300

    fix: console.log ajust

--- assets/js/category.product.js
+++ assets/js/category.product.js
@@ -1,117 +1,137 @@
-// each category haves a lot of products
-// each product haves one categiory
-
 class Category{
-    constructor(id, name){
-        this.id = id;
-        this.name = name;
-        this.products = []
+    constructor(id,name){
+        this.id= id;
+        this.name= name;
+        this.products= [];
     }
 }
-class Product {
-    constructor(id, name, price, category){
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.category = category;
-    }
 
-}
-class CategoryService {
-    constructor(){
-        this.categories =[];
-        this.nextCategoryId = 1
+class Product{
+    constructor(id, name, price, category){
+        this.id=id;
+        this.name=name;
+        this.price=price;
+        this.category=category;
     }
+}
+class CategoryService{
+    constructor(){
+        this.categories= [];
+        this.nextCategoryId= 1;
+    }
+//CREUD =>
+// C => create
     addCategory(name){
-        const id = this.nextCategoryId;
-        this.nextCategoryId++;
+        const id= this.nextCategoryId;
+        this.nextCategoryId ++;
 
         const category = new Category(id, name);
         this.categories.push(category);
     }
 
-    getCategoryById(id){
+    //R=> read
+    getCategoryCategoryById(id){
         return this.categories.find((category)=> category.id == id );
     }
-    updateCategory(id, name){
-        const category = this.getCategoryById(id);
-        category.name = name;
+
+    //U=> update
+    UpdateCategory(id, name){
+        const category= this.getCategoryCategoryById(id);
+        category.name= name;
     }
+
+    //D=> delet
     deleteCategory(id){
-        const category = this.getCategoryById(id);
-        const index = this.categories.indexOf(category);
+        const category= this.getCategoryCategoryById(id);
+        const index= this.categories.indexOf(category);
 
         this.categories.splice(index, 1);
     }
 }
-class ProductService{
-    constructor(){
-        this.products = [];
-        this.nextProductId = 1;
+
+
+
+    class ProductService{
+        constructor() {
+            this.products=[];
+            this.nextProductiId= 1;
     }
+
     addProduct(name, price, category){
-        const id = this.nextProductId;
-        this.nextCategoryId++;
+        const id= this.nextProductiId;
+        this.nextProductiId++;
 
-        const product = new Product(id, name, price, category);
-
+        const product = new Product(id,name, price, category);
         this.products.push(product);
         category.products.push(product);
-        createProduct();
     }
+    //R=> read
     getProductById(id){
-        return this.products.find((product) => product.id ==id);
-
+        return this.products.find((product)=> product.id == id );
     }
-   
-}
 
-const categoryList = new CategoryService();
-const productsList = new  ProductService();
+}
+const categoriesList = new CategoryService();
+const procutsList= new ProductService();
+
 function createCategory(){
-    const categoryName = 'Candies';
-    const categoryName2 = 'Shoes';
-    const categoryName3 = 'Books';
+    const categoryName= "Candies";
+    const categoryName1= "Shoes";
+    const categoryName2= "Book";
 
-    categoryList.addCategory(categoryName2)
-    categoryList.addCategory(categoryName3)
-    categoryList.addCategory(categoryName)
-   // console.log(categoryList.categories)
+    categoriesList.addCategory(categoryName);
+    categoriesList.addCategory(categoryName1);
+    categoriesList.addCategory(categoryName2);
 
+    //console.log(categorieslist.categories);
+    console.log("Categorias criadas!");
 }
+
+// C => create
 function createProduct(){
-    const productName1 = "Lindt"
-    const productPrice1 = 30;
-    const productCategory1 = categoryList.categories[1];
+    const procutName= "Choco";
+    const procutPrice= 0.50;
+    const procutCategory =categoriesList.categories[0];
+    
+    const procutName1= "Sneakers";
+    const procutPrice1= 100;
+    const procutCategory1 =categoriesList.categories[1];
+   
+    const procutName2= "Harry Potter";
+    const procutPrice2= 50;
+    const procutCategory2 =categoriesList.categories[2];
 
-    const productName2 = "Sneaker"
-    const productPrice2 = 30;
-    const productCategory2 = categoryList.categories[2];
+    procutsList.addProduct(procutName,procutPrice,procutCategory);
+    procutsList.addProduct(procutName1,procutPrice1,procutCategory1);
+    procutsList.addProduct(procutName2,procutPrice2,procutCategory2);
 
-    const productName3 = "Red, White and Royal Blue"
-    const productPrice3 = 30;
-    const productCategory3 = categoryList.categories[3];
-
-    productsList.addProduct(productName1, productPrice1, productCategory1 )
-    productsList.addProduct(productName2, productPrice2, productCategory2 )
-    productsList.addProduct(productName3, productPrice3, productCategory3 )
+    console.log(procutsList.products);
 }
+
+ //R=> read
 function findCategory(id){
-    const category = categoryList.getCategoryById(id);
+    const category= categoriesList.getCategoryCategoryById(id);
 
     console.log(category.name);
-}                   
-function editCategory(id, name){
-    categoryList.updateCategory(id,name);
-    console.log(categoryList.categories);
-}                                
-function deleteCategory(id){
-    categoryList.deleteCategory(id);
-    console.log(categoryList.categories);
 }
 
-function findProduct(id){
-    const product = productsList.getProductById(id);
+//U=> update
+function editCategory(id,name){
+    categoriesList.UpdateCategory(id,name);
+
+    console.log(categoriesList.categories);
+}
+
+//D=> delet
+function deleteCategory(id){
+    categoriesList.deleteCategory(id);
+
+    console.log(categoriesList.categories);
+}
+
+function findProductById(id){
+    const product= procutsList.getProductById(id);
 
     console.log(product);
 }
+
